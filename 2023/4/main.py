5b26890ffe257ab4e609ca9fe4e53b664b450ed1
Kevin <kevin@magicmonkey.org>
2023-12-04 15:07:35 +0000

    Day 4 part 2 complete

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -1,11 +1,11 @@
 import re
+from pprint import pprint
 
 def parse(fname: str):
     cards = {}
     with open(fname) as file:
         for line in file:
             row = line.strip()
-            print(row)
             bits = re.findall('^Card +(\d+): ([^\|]*) \| (.*)$', row)
             for bit in bits:
                 card = {
@@ -15,24 +15,53 @@ def parse(fname: str):
                 yield card
 
 
+def num_wins(card) -> int:
+    count = 0
+    for winning_num in card['winning']:
+        if winning_num in card['have']:
+            count += 1
+    return count
+
+
 def part1(fname: str):
     total = 0
     for card in parse(fname):
         score = 0
-        for winning_num in card['winning']:
-            if winning_num in card['have']:
-                print(f"Got a match for {winning_num}")
-                if score == 0:
-                    score = 1
-                else:
-                    score *= 2
+        n = num_wins(card)
+        if n == 0:
+            score = 0
+        elif n == 1:
+            score = 1
+        else:
+            score = 2 ** (n-1)
         total += score
-        print(f"{card['have']} - {card['winning']} - {score} - {total}")
-        print("")
     print(total)
+
+
+def part2(fname: str):
+    total = 0
+    cards = []
+
+    for card in parse(fname):
+        n = num_wins(card)
+        card['wins'] = n
+        card['instances'] = 1
+        cards.append(card)
+
+    for c in range(len(cards)):
+        for i in range(cards[c]['wins']):
+            cards[c+i+1]['instances'] += 1 * cards[c]['instances']
+
+    count = 0
+    for card in cards:
+        count += card['instances']
+
+    pprint(count)
 
 
 if __name__ == '__main__':
     #part1("test.txt")
-    part1("input.txt")
+    #part1("input.txt")
+    #part2("test.txt")
+    part2("input.txt")
 
