d3d542125d6235ac6da47b88784a0270f9f52a00
Kevin Bowman <kevin@magicmonkey.org>
2023-12-09 10:29:12 +0000

    Day 9 part 2 complete

--- 2023/9/main.py
+++ 2023/9/main.py
@@ -20,6 +20,22 @@ def get_next(nums):
     return gaps[0][len(gaps[0])-1]
 
 
+def get_prev(nums):
+    gaps = [nums]
+    while True:
+        next_gaps = get_gaps(gaps[len(gaps)-1])
+        gaps.append(next_gaps)
+        if len(set(next_gaps)) == 1:
+            break
+
+    # Now unroll the left-hand-side of all of the gaps
+    thing = 0
+    gaps.append([0] * (len(gaps[len(gaps)-1])-1))
+    for i in range(len(gaps), 0, -1):
+        gaps[i-2].insert(0, gaps[i-2][0] - gaps[i-1][0])
+    return gaps[0][0]
+
+
 def part1(fname: str):
     with open(fname) as file:
         counter = 0
@@ -28,7 +44,20 @@ def part1(fname: str):
             counter += get_next(nums)
     print(counter)
 
+
+def part2(fname: str):
+    with open(fname) as file:
+        counter = 0
+        for line in file:
+            nums = [int(x) for x in line.split(" ")]
+            prev = get_prev(nums)
+            counter += prev
+    print(counter)
+
+
 if __name__ == '__main__':
-    part1("test.txt")
-    part1("input.txt")
+    #part1("test.txt")
+    #part1("input.txt")
+    #part2("test.txt")
+    part2("input.txt")
 
